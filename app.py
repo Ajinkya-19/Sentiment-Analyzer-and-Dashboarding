88b7178cf7a734e42d85ba5f798c1dae52f220fd
Ajinkya Chavan <ajinkyac7498@gmail.com>
2024-12-19 21:24:27 +0530

    Initial commit

--- app.py
+++ app.py
@@ -16,8 +16,20 @@ nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
 nltk.data.path.append(nltk_data_dir)
 
 # Ensure NLTK resources are available
-nltk.download('punkt', download_dir=nltk_data_dir, quiet=True)
-nltk.download('stopwords', download_dir=nltk_data_dir, quiet=True)
+#nltk.download('punkt', download_dir=nltk_data_dir, quiet=True)
+#nltk.download('stopwords', download_dir=nltk_data_dir, quiet=True)
+
+# Ensure punkt tokenizer is available
+nltk_data_path = os.path.join(os.getcwd(), 'nltk_data')
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+
+nltk.data.path.append(nltk_data_path)
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
 
 # Sentiment Analysis Preprocessing
 ps = PorterStemmer()
